6a0e381e0dde3dcba624ecbcca704b828e4da211
MAYORQUIN84 <erickmayorquin1984@gmail.com>
2016-09-28 22:07:24 -0500

    laboratorio3
    juan

---
+++ laboratorio3/Juanelcallado/Juan.py
@@ -0,0 +1,31 @@
+print("hola me llamo juan")
+
+a = 'ofi'
+b = 'chilea'
+c = 'Mmm'
+d = 'me da igual'
+bandera = True
+carac = input()
+
+
+if carac.endswith('?'):
+    print(a)
+elif carac.isupper():
+    print (b)
+elif carac.isalnum():
+    print(d)
+else:
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
